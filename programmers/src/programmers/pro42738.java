8f75994558ca1accb408d3ac3631f2ec06ff3752
Samdasoo1076 <dlwlalsdkd@gmail.com>
2023-10-11 16:32:38 +0900

    2023-10-11
    2023-10-11 푼 문제 소스 코드 추가

---
+++ programmers/src/programmers/pro42738.java
@@ -0,0 +1,23 @@
+package programmers;
+
+import java.util.*;
+public class pro42738 {
+
+	public static void main(String[] args) {
+		int[] array = {1, 5, 2, 6, 3, 7, 4};
+		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+		int[] answer = new int[commands.length];
+		
+		for(int i = 0; i < commands.length; i++) {
+			int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+			Arrays.sort(temp);
+			answer[i] = temp[commands[i][2] - 1];
+		}
+		
+		
+		for(int i = 0; i < array.length; i++) {
+				System.out.print(answer[i] + ", ");
+		}
+	}
+
+}
