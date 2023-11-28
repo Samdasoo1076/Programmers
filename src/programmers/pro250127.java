ae2ce7074e2b638108bb35a24f7116a68f10e9ae
Samdasoo1076 <dlwlalsdkd@gmail.com>
2023-11-28 11:43:21 +0900

    2023-11-28
    점심까지 풀은 문제 소스 코드 추가

--- src/programmers/pro250127.java
+++ src/programmers/pro250127.java
@@ -4,7 +4,7 @@ public class pro250127 {
 	
 	public int func1(int humidity, int val_set) {
 		if(humidity < val_set) 
-			return humidity;
+			return 3;
 		return 1;
 	}
 	
@@ -22,27 +22,40 @@ public class pro250127 {
         else if(humidity <= 10) 
         	return 5;
 		return humidity;
+		
 	}
 	
 	public int func3(int humidity, int val_set) {
 		if(humidity < val_set)
 			return 1;
-		return 3;
+		return 0;
 	}
 	
-	public static void main(String[] args) {
-		String mode_type = "auto";
-		int humidity = 23;
-		int val_set = 45;
+	public int solution(String mode_type, int humidity, int val_set) {
 		int answer = 0;
 		
-		if(mode_type.equals("auto")) {
-			answer = func2;
-		} else if(mode_type.equals("target")) {
-			answer = func1;
-		} else if(mode_type.equals("minimum")) {
-			answer = func3;
-		}
+		if (mode_type.equals("auto")) {
+            answer = func2(humidity);
+        } else if (mode_type.equals("target")) {
+            answer = func1(humidity, val_set);
+        } else if (mode_type.equals("minimum")) {
+            answer = func3(humidity, val_set);
+        }
+		
+		return answer;
 	}
 
+	public static void main(String[] args) {
+		pro250127 solution = new pro250127();
+		
+		String mode_type = "minimum";
+		int humidity = 10;
+		int val_set = 34;
+		
+		int result = solution.solution(mode_type, humidity, val_set);
+		
+		System.out.println(result);
+		
+		
+	}
 }
